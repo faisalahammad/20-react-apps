69405a95af1daf77b35b27a448921d5a63208049
Faisal Ahammad <faisalahammad24@gmail.com>
2022-01-30 12:23:10 +0600

    01-promodoro - Time countdown & reset function added

--- 01-promodoro/src/App.js
+++ 01-promodoro/src/App.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+// Add extra zero at the begining of the min/sec
+function padTime(time) {
+  return time.toString().padStart(2, '0');
+}
+
 function App() {
+  const [title, setTitle] = useState('Let the countdown begin!!!');
+  const [timeLeft, setTimeLeft] = useState(10);
+
+  // function
+  function startTimer() {
+    setInterval(() => {
+      setTimeLeft(timeLeft => {
+        if (timeLeft >= 1) return timeLeft - 1;
+
+        // reset the timer
+        return 0;
+      });
+    }, 1000)
+  }
+
+  // calculation
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
+
   return (
     <div className="app">
-      <h2>Pomodoro!</h2>
+      <h2>{ title }</h2>
 
       <div className="timer">
-        <span>00</span>
+        <span>{ minutes }</span>
         <span>:</span>
-        <span>00</span>
+        <span>{ seconds }</span>
       </div>
 
       <div className="buttons">
-        <button>Start</button>
+        <button onClick={startTimer}>Start</button>
         <button>Stop</button>
         <button>Reset</button>
       </div>
