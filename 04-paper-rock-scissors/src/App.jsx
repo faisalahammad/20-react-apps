953f907e40a8d7336283cdea6038d8800f97449d
Faisal Ahammad <faisalahammad24@gmail.com>
2022-04-01 20:58:21 +0600

    04 - Paper Rock Scissors (Ready to deploy)

--- 04-paper-rock-scissors/src/App.jsx
+++ 04-paper-rock-scissors/src/App.jsx
@@ -1,10 +1,57 @@
-import React from 'react';
-import './App.css';
-import Paper from './icons/Paper';
-import Rock from './icons/Rock';
-import Scissors from './icons/Scissors';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Paper from "./icons/Paper";
+import Rock from "./icons/Rock";
+import Scissors from "./icons/Scissors";
+
+const choices = [
+  { id: 1, name: "rock", component: Rock, losesTo: 2 },
+  { id: 2, name: "paper", component: Paper, losesTo: 3 },
+  { id: 3, name: "scissors", component: Scissors, losesTo: 1 },
+];
 
 export default function App() {
+  const [wins, setWins] = useState(0);
+  const [losses, setLosses] = useState(0);
+  const [userChoice, setUserChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [gameState, setGameState] = useState(null);
+
+  useEffect(() => {
+    restartGame();
+  }, []);
+
+  function restartGame() {
+    setGameState(null);
+    setUserChoice(null);
+
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    setComputerChoice(randomChoice);
+  }
+
+  const handleUserChoice = (choice) => {
+    const chosenChoice = choices.find((c) => c.id === choice);
+    setUserChoice(chosenChoice);
+
+    // determine the winner
+    setGameState("Win");
+
+    if (chosenChoice.losesTo === computerChoice.id) {
+      setGameState("lose");
+      setLosses((losses) => losses + 1);
+    } else if (computerChoice.losesTo === chosenChoice.id) {
+      setGameState("win");
+      setWins((wins) => wins + 1);
+    } else if (computerChoice.id === chosenChoice.id) {
+      setGameState("draw");
+    }
+  };
+
+  function renderComponent(choice) {
+    const Component = choice.component;
+    return <Component />;
+  }
+
   return (
     <div className="app">
       {/* information goes here */}
@@ -14,19 +61,37 @@ export default function App() {
         {/* wins vs losses stats */}
         <div className="wins-losses">
           <div className="wins">
-            <span className="number">0</span>
-            <span className="text">Wins</span>
+            <span className="number">{wins}</span>
+            <span className="text">{wins <= 1 ? "Win" : "Wins"}</span>
           </div>
 
           <div className="losses">
-            <span className="number">0</span>
-            <span className="text">Losses</span>
+            <span className="number">{losses}</span>
+            <span className="text">{losses <= 1 ? "Lose" : "Losses"}</span>
           </div>
         </div>
       </div>
 
       {/* the popup to show win/loss/draw */}
-      {/* <div className="game-state"></div> */}
+      {gameState && (
+        <div
+          className={`game-state ${gameState}`}
+          onClick={() => restartGame()}
+        >
+          <div>
+            <div className="game-state-content">
+              <p>{renderComponent(userChoice)}</p>
+              {gameState === "win" && <p>Congrates! You win.</p>}
+              {gameState === "lose" && <p>Oops! You lose.</p>}
+              {gameState === "draw" && <p>You draw.</p>}
+
+              <p>{renderComponent(computerChoice)}</p>
+            </div>
+
+            <button>Play Again</button>
+          </div>
+        </div>
+      )}
 
       <div className="choices">
         {/* choices captions */}
@@ -36,13 +101,13 @@ export default function App() {
 
         {/* buttons for my choice */}
         <div>
-          <button className="rock">
+          <button className="rock" onClick={() => handleUserChoice(1)}>
             <Rock />
           </button>
-          <button className="paper">
+          <button className="paper" onClick={() => handleUserChoice(2)}>
             <Paper />
           </button>
-          <button className="scissors">
+          <button className="scissors" onClick={() => handleUserChoice(3)}>
             <Scissors />
           </button>
         </div>
