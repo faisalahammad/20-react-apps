92585c269d57ff3b1049dbc78f8417b0d3c4e263
Faisal Ahammad <faisalahammad24@gmail.com>
2022-04-24 00:04:26 +0600

    06-image-gallery: Infite scrolling added

--- 06-infinite-image-gallery/src/App.jsx
+++ 06-infinite-image-gallery/src/App.jsx
@@ -1,18 +1,26 @@
 import React, { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import "./App.css";
 
 const accessKey = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
 
 function App() {
   const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    fetch(`https://api.unsplash.com/photos/?client_id=${accessKey}`)
+    getPhotos();
+  }, [page]);
+
+  function getPhotos() {
+    fetch(
+      `https://api.unsplash.com/photos/?client_id=${accessKey}&page=${page}`
+    )
       .then((res) => res.json())
       .then((data) => {
-        setImages(data);
+        setImages((images) => [...images, ...data]);
       });
-  }, []);
+  }
 
   if (!accessKey) {
     return (
@@ -31,13 +39,35 @@ function App() {
         <button>Search</button>
       </form>
 
-      <div className="image-grid">
-        {[...Array(100)].map((_, index) => (
-          <div className="image" key={index}>
-            <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
-          </div>
-        ))}
-      </div>
+      <InfiniteScroll
+        dataLength={images.length}
+        next={() => setPage((page) => page + 1)}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <div className="image-grid">
+          {images.map((image, index) => (
+            <a
+              className="image"
+              key={index}
+              href={image.links.html}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={image.urls.regular}
+                alt={image.alt_description}
+                loading="lazy"
+              />
+            </a>
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 }
